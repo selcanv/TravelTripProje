21b44892d23df6888c9e1b2a69cf9ef7f88910ee
Selcan Vuruk <Selcanvuruk17@gmail.com>
2024-05-04 19:51:45 +0300

    Add project files.

---
+++ TravelTripProje/Models/Siniflar/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class Context : DbContext
+    {
+        public DbSet <Admin> Admins { get; set; }  //dbcontext ten miras aldığım için databaseset (dbset) geliyor <Admin> sınıfı kullanmak istiyorum veriranabında da bu admins (peluayz) olacak 
+        public DbSet <AdresBlog> AdresBlogs { get; set; }
+        public DbSet <Blog> Blogs { get; set; }
+        public DbSet <Hakkimizda> Hakkimizdas { get; set; }
+        public DbSet <Iletisim> Iletisims { get; set; }
+        public DbSet <Yorumlar> Yorumlars { get; set; }
+       
+    }
+}
